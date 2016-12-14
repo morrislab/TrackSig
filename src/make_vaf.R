d54e894719c7f44c04a00f8b0707606bfec84936
Yulia Rubanova <julia_erise@mail.ru>
2016-12-13 20:01:22 -0500

    make_vaf.R done

--- src/make_vaf.R
+++ src/make_vaf.R
@@ -1,68 +1,62 @@
-vcf_dir = "/home/q/qmorris/yulia/pwgs/consprelim/"  
-vaf_dir = "/scratch/q/qmorris/yulia/pwgs/ssmvaf_full/"
-phi_dir = "/home/q/qmorris/yulia/pwgs/ssmphi/consprelim.sample-5000.psub/"
+args <- commandArgs(trailingOnly = TRUE)
+
+vcf_dir = "data/vcf/"  
+vaf_dir = "data/phi/"
+
+
+if (length(args) > 0) {
+	vcf_dir <- paste0(args[1], "/")
+}
+
+if (length(args) > 1) {
+	vaf_dir <- paste0(args[2], "/")
+}
+
+print(paste("VCF dir:", vcf_dir))
+print(paste("VAF dir:",vaf_dir))
 
 get_vaf <- function(info)
 {
   info <- strsplit(info, ";")[[1]]
+
   vaf <- as.double(gsub("VAF=(.*)", "\\1", info[grep("VAF=(.*)", info)]))
   vaf <- vaf * 2
   
   return(vaf)
 }
 
-make_vafs_for_mutations <- function()
+make_vafs_for_mutations <- function(vcf_dir, vaf_dir)
 {
   for (f in list.files(vcf_dir))
   {
-    tumor_id <- gsub("(.*).annotated.snv_mnv.vcf", "\\1", f)
+    tumor_id <- gsub("(.*).vcf", "\\1", f)
       
     vcf <- read.delim(paste0(vcf_dir, f), header = T, stringsAsFactors=F)
     info <- vcf[,ncol(vcf)]
-    
-    filter <-  vcf[,"FILTER"] == "."
+
+    if (sum(is.na(info) > 0)) {
+      stop("VCF file does not contain VAF info")
+    }
+
+    if (sum(grepl("VAF=(.*)", info)) == 0) {
+      stop("VCF file does not contain VAF info")
+    }
+
+    filter <-  vcf[,"FILTER"] == "." & grepl("VAF=(.*)", info)
     
     vaf_data <- paste(vcf[,c("X.CHROM")], vcf[,c("POS")], sep="_")
     vaf_values <- as.matrix(sapply(info, get_vaf), ncol=1)
+
     empty_vaf <- sapply(vaf_values, length) == 1
     
     vaf_data <- paste(vaf_data, vaf_values, sep="=")
     vaf_data <- vaf_data[filter & empty_vaf]
     
-    
+    if (!file.exists(vaf_dir)) {
+    	dir.create(vaf_dir)
+    }
     write.table(vaf_data, file=paste0(vaf_dir, tumor_id, ".txt"), col.names=F, row.names = F, quote=F)
   }
 } 
 
-make_vafs_for_mutations_subset_with_phi  <- function()
-{
-  # Make vaf counts only for tumors and mutations where phi is available.
-  for (f in list.files(vcf_dir)[2771:length(list.files(vcf_dir))])
-  {
-    tumor_id <- gsub("(.*).annotated.snv_mnv.vcf", "\\1", f)
-    print(paste(tumor_id, " Sample ", which( list.files(vcf_dir) == f), " out of ", length( list.files(vcf_dir))))
-    # Skipping tumors where we don't have phis
-    if (!file.exists(paste0(phi_dir, tumor_id, ".txt"))) {
-      print("Skipping...")
-      next
-    }
-    
-    vcf <- read.delim(paste0(vcf_dir, f), header = T, stringsAsFactors=F)
-    info <- vcf[,ncol(vcf)]
-    
-    phis <- read.delim(paste0(phi_dir, tumor_id, ".txt"), header = F, stringsAsFactors=F, sep="=")
-    
-    filter <-  vcf[,"FILTER"] == "."
-    
-    vaf_data <- paste(vcf[,c("X.CHROM")], vcf[,c("POS")], sep="_")
-    filter_no_phi <- vaf_data %in% phis[,1]
-      
-    vaf_values <- as.matrix(sapply(info, get_vaf), ncol=1)
-    empty_vaf <- sapply(vaf_values, length) == 1
-    
-    vaf_data <- paste(vaf_data, vaf_values, sep="=")
-    vaf_data <- vaf_data[filter & empty_vaf & filter_no_phi]
-    
-    write.table(vaf_data, file=paste0(vaf_dir, tumor_id, ".txt"), col.names=F, row.names = F, quote=F)
-  }
-} 
+make_vafs_for_mutations(vcf_dir, vaf_dir)
